bc0479ee6cf6318c836044e2d70ceb0067fea51e
Nick <nick.buscemi13@gmail.com>
2023-05-15 12:06:20 -0400

    finished

--- server.js
+++ server.js
@@ -68,44 +68,109 @@ const server = http.createServer((req, res) => {
       //res.end(JSON.stringify({ roomDescription: player.currentRoom.description}));
     } 
 
-    // Phase 3: GET /rooms/:roomId
-    if (req.method === "GET" && req.url.startsWith('/rooms/')) {
-      const roomId = req.url.split('/')[2];
-      if (!roomId) {
-        res.statusCode = 400;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Room ID not provided');
-      }
+    // helper function for phase 3 and 4
+    const handleRoomRequest = (roomId, res) => {
       const room = world.rooms[roomId];
-      let roomName = room.name;
-      let inventory = player.items.map(item => `<p>${item.name}: ${item.description}`).join('');
-      let roomItems = room.items.map(item => `<p>${item.name}</p>`);
-      let exits = room.exitsToString();
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'text/html');
-      let htmlContent = fs.readFileSync('views/room.html', 'utf-8');
-      htmlContent = htmlContent
-        .replace(/#{roomName}/g, roomName)
-        .replace('#{inventory}', inventory)
-        .replace('#{roomItems}', roomItems)
-        .replace('#{exits}', exits)
-      res.end(htmlContent);
+      if (room) {
+        let roomName = room.name;
+        let inventory = player.inventoryToString();
+        let roomItems = room.itemsToString();
+        let exits = room.exitsToString();
+        let htmlContent = fs.readFileSync('views/room.html', 'utf-8');
+        htmlContent = htmlContent
+          .replace(/#{roomName}/g, roomName)
+          .replace('#{inventory}', inventory)
+          .replace('#{roomItems}', roomItems)
+          .replace('#{exits}', exits);
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/html');
+        res.end(htmlContent);
+      } else {
+        res.statusCode = 404;
+        res.end('Room not found');
+      }
+    };
+
+    // Phase 3: GET /rooms/:roomId
+    if (req.method === "GET" && req.url.startsWith('/rooms/') && req.url.split('/').length === 3) {
+      const roomId = req.url.split('/')[2];
+      handleRoomRequest(roomId, res);
     }
 
     // Phase 4: GET /rooms/:roomId/:direction
-    if (req.method === "GET" && req.url.startsWith('/rooms/')) {
-      const roomId = req.url.split('/')[2];
-      const direction = req.url.split('/')[3];
-      if (!roomId || !direction) {
-        res.statusCode = 400;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Room ID and/or direction not provided');
-      }
+// Phase 4: GET /rooms/:roomId/:direction
+    if (req.method === "GET" && req.url.startsWith('/rooms/') && req.url.split('/').length === 4) {
+      const roomId = req.url.split('/')[2]; // (1,2,3,4,5)
+      const direction = req.url.split('/')[3][0]; // (c,n,e,w,s)
       
-    
+      
+      if (roomId == player.currentRoom.id) {
+        player.move(direction);
+      } 
+      res.writeHead(302, {'Location': `/rooms/${player.currentRoom.id}`});
+      res.end();
+      return;
+      
     }
 
+
+    
     // Phase 5: POST /items/:itemId/:action
+    if (req.method === "POST" && req.url.startsWith('/items/') && req.url.split('/').length === 4) {
+      const itemId = req.url.split('/')[2];
+      const action = req.url.split('/')[3];
+      
+      // Find the item either in the room or in the player's inventory
+      const item = player.currentRoom._getItem(itemId) || player._getItem(itemId);
+      
+      // If the item does not exist in either the room or the player's inventory
+      if (!item) {
+        res.statusCode = 404;
+        res.end('Item not found');
+        return;
+      }
+
+      try {
+        switch (action) {
+          case 'take':
+            if (!player.currentRoom._getItem(itemId)) {
+              res.statusCode = 404;
+              res.end('Item not found in room');
+            } else {
+              player.takeItem(itemId);
+              res.writeHead(302, {'Location': `/rooms/${player.currentRoom.id}`});
+              res.end();
+            }
+            break;
+          case 'drop':
+            if (!player._getItem(itemId)) {
+              res.statusCode = 404;
+              res.end('Item not found in player inventory');
+              return;
+            } else {
+              player.dropItem(itemId);
+              res.writeHead(302, {'Location': `/rooms/${player.currentRoom.id}`});
+              res.end();
+            }
+            break;
+          default:
+            res.statusCode = 400;
+            res.end('Invalid action');
+        }
+      } catch (error) {
+        console.error(error);
+        res.writeHead(302, {'Location': `/rooms/${player.currentRoom.id}`});
+        res.end();
+      }
+    }
+
+        
+          
+          
+          
+          
+          
+      
 
     // Phase 6: Redirect if no matching route handlers
   })
